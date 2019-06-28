0986f54d1cde33219f26fc59e2412ee1effaa994
Colin <cstatro@gmail.com>
2019-06-28 19:53:21 -0400

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
 class Animal
+    attr_accessor :weight
+    attr_reader :species, :nickname
+    @@all = []
+    def initialize species,weight,nickname
+        @species,@weight,@nickname  = species,weight,nickname  
+         Animal.all << self
+    end
+    def self.all 
+        @@all
+    end
+
+    def zoo 
+        Zoo.all.select {|z| z.animals.any? self}[0]
+    end
+    def self.find_by_species  species
+        Animal.all.select {|animal| animal.species == species}
+    end
 
 end
+
+
+
--- run.rb
+++ run.rb
@@ -3,6 +3,21 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
+animal1 = Animal.new("Cat",12,"Garfield")
+animal2 = Animal.new("Dog",12,"Boboo")
+animal3 = Animal.new("Cat",12,"Mr. Bigglesworth")
+
+zoo1 = Zoo.new("Bronx Zoo", "Bronx")
+zoo2 = Zoo.new("Matt Damon's Stupid Zoo", "Bad Movie")
+zoo3 = Zoo.new("San Diego Zoo", "California")
+
+zoo1.add_animal animal1
+zoo1.add_animal animal2
+
+
+
+
+
 #Test your code here
 
 
